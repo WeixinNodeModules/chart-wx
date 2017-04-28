2c0a45d70533b6d1b9ecc7c577efb5188df4664b
xianda.zheng <xianda.zheng@baidao.com>
2017-04-28 16:09:07 +0800

    添加 k线数据模型

---
+++ kLineItemStruct.js
@@ -0,0 +1,30 @@
+var IsNumber = require('lodash.IsNumber')
+var Moment = require('moment')
+var Percent = require('percent')
+
+module.exports = function KLineItemStruct(quotationData = {
+  open = 0,
+  high = 0,
+  low = 0,
+  close = 0,
+  value = 0,
+  volume = 0,
+  avgclose = 0,
+  updateTime = '',
+  status = false,
+  preClose = 0
+}) {
+  this.open = quotationData.open
+  this.high = quotationData.high
+  this.low = quotationData.low
+  this.close = quotationData.close
+  this.value = quotationData.value
+  this.volume = quotationData.volume
+  this.avgclose = quotationData.avgClose
+  this.updateTime = quotationData.updateTime
+  this.status = quotationData.status
+  this.preClose = quotationData.preClose
+
+  this.upDrop = this.close - this.preClose
+  this.percent = Percent(this.upDrop, this.preClose)
+}
