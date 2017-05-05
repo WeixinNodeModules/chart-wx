fc86354167566c000cef4baed8ead7a63687899f
xianda.zheng <xianda.zheng@baidao.com>
2017-05-05 17:24:15 +0800

    添加 between 方法

--- utils.js
+++ utils.js
@@ -2,6 +2,11 @@ function percent(numerator, denominator) {
   return (Math.round(numerator / denominator * 10000) / 100.00).toFixed(2) + "%" // 小数点后两位百分比
 }
 
+function between(min, max, value) {
+  return Math.min(Math.max(value, min), max)
+}
+
 module.exports = {
-  percent: percent
+  percent: percent,
+  between: between
 }
